fe3f21a6b7a2dffd29ebf18f9864b43ea27aee10
Luizprates10bm <luizgustavoaugustoprates@gmail.com>
2026-02-02 22:56:53 -0300

    Delete estrutura_de_repeticao_while.py

--- estrutura_de_repeticao_while.py
+++
@@ -1,9 +0,0 @@
-senha = input ("Digite sua senha: ")
-senha_correta = "123"
-
-while senha != senha_correta: 
-    print("senha incorreta! Tente novamente")
-    senha = input ("Digite sua senha: ")
-
-
-print("Bem vindo ao sistema!")
